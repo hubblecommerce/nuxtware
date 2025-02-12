f924092a7ae14a1319e569f71158263a82aaab73
Lukas Heinze <heinze@digitalmanufaktur.com>
2025-02-12 16:26:20 +0100

    Developed dropdown component and use it to render category filters

---
+++ app/components/component/ComponentDropdown.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="relative">
+        <FoundationButton
+            :id="triggerId"
+            ref="triggerEl"
+            :class="[
+                'gap-2',
+                { 'opacity-50 cursor-not-allowed': disabled }
+            ]"
+            :aria-haspopup="true"
+            :aria-expanded="isOpen"
+            :disabled="disabled"
+            un-styled
+            v-bind="$attrs"
+            @click="toggleDropdown"
+            @keydown.esc="closeDropdown"
+            @keydown.arrow-down.prevent="openDropdown"
+        >
+            {{ $t(triggerLabel) }}
+            <FoundationIcon v-show="isOpen" name="chevron-up" />
+            <FoundationIcon v-show="!isOpen" name="chevron-down" />
+        </FoundationButton>
+
+        <transition
+            enter-active-class="transition ease-out duration-100"
+            enter-from-class="transform opacity-0 scale-95"
+            enter-to-class="transform opacity-100 scale-100"
+            leave-active-class="transition ease-in duration-75"
+            leave-from-class="transform opacity-100 scale-100"
+            leave-to-class="transform opacity-0 scale-95"
+        >
+            <div
+                v-if="isOpen"
+                ref="contentEl"
+                :class="[
+                  contentClasses
+                ]"
+                role="menu"
+                :aria-labelledby="triggerId"
+                @keydown.stop.esc="closeDropdown"
+            >
+                <slot />
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onClickOutside } from '@vueuse/core'
+import { useFocusTrap } from '@vueuse/integrations/useFocusTrap'
+
+interface Props {
+    triggerLabel: string;
+    disabled?: boolean;
+    contentClasses?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    disabled: false,
+    contentClasses: '',
+})
+
+const emit = defineEmits<{
+    (e: 'open' | 'close'): void;
+}>()
+
+defineOptions({
+    inheritAttrs: false
+})
+
+const isOpen = ref(false)
+const uuId = useId()
+const triggerId = `dropdown-trigger-${uuId}`
+const triggerEl = ref()
+const contentEl = ref()
+
+onClickOutside(contentEl, () => closeDropdown(), { ignore: [triggerEl] })
+const { activate, deactivate } = useFocusTrap(contentEl, { allowOutsideClick: true })
+
+function toggleDropdown () {
+    if (!props.disabled) {
+        isOpen.value = !isOpen.value
+        if (isOpen.value) {
+            emit('open')
+            nextTick(() => {
+                activate()
+            })
+        } else {
+            emit('close')
+            deactivate()
+        }
+    }
+}
+
+function openDropdown () {
+    if (!isOpen.value && !props.disabled) {
+        isOpen.value = true
+        emit('open')
+        nextTick(() => {
+            activate()
+        })
+    }
+}
+
+function closeDropdown () {
+    if (isOpen.value) {
+        isOpen.value = false
+        emit('close')
+        deactivate()
+    }
+}
+</script>
--- app/components/product/ProductListingFilters.vue
+++ app/components/product/ProductListingFilters.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { type UnwrapNestedRefs } from 'vue'
-import type { CmsElementSidebarFilter } from "@shopware-pwa/composables-next";
-import type { RequestParameters } from "#shopware";
-import { getTranslatedProperty } from "@shopware-pwa/helpers-next";
+import type { UnwrapNestedRefs } from 'vue'
+import type { CmsElementSidebarFilter } from '@shopware-pwa/composables-next'
+import type { RequestParameters } from '#shopware'
+import { getTranslatedProperty } from '@shopware-pwa/helpers-next'
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
 
 const props = defineProps<{
     content: CmsElementSidebarFilter
@@ -50,7 +51,7 @@ const searchCriteriaForRequest: ComputedRef<RequestParameters<"searchPage">> =
         rating: selectedFilters["rating"],
         search: "",
         limit: route.query.limit ? Number(route.query.limit) : 15,
-    }));
+    }))
 
 // Set selected filters from query
 for (const param in route.query) {
@@ -65,7 +66,7 @@ for (const param in route.query) {
                 selectedFilters[param].push(element)
             });
         } else {
-            selectedFilters[param] = route.query[param];
+            selectedFilters[param] = route.query[param]
         }
     }
 }
@@ -82,44 +83,69 @@ async function executeSearch () {
         query,
     });
 }
+
+const open = ref(false)
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const lgAndLarger = breakpoints.greaterOrEqual('lg')
 </script>
 
 <template>
-    <client-only>
-        <div v-if="getInitialFilters.length" class="flex gap-3 border">
+    <FoundationButton color="primary" class="w-full lg:hidden" @click="open = true">
+        Filter
+    </FoundationButton>
+    <LayoutSidenavOverlay v-model="open" :unwrap="lgAndLarger" class="bg-white">
+        <div class="flex justify-between items-center p-2 pr-0 lg:hidden">
+            <FoundationHeadline tag="h2" class="text-lg font-semibold">Filter</FoundationHeadline>
+            <FoundationButton
+                ref="sidenavMenuCloseButton"
+                size="medium"
+                variant="ghost"
+                square
+                @click="open = false"
+            >
+                <span class="sr-only">{{ $t('sidenav.menu.mobile.close') }}</span>
+                <FoundationIcon name="x" />
+            </FoundationButton>
+        </div>
+        <div v-if="getInitialFilters.length" class="flex flex-col gap-3 lg:flex-row lg:flex-wrap">
             <div
                 v-for="filter in getInitialFilters"
                 :key="`${filter?.id || filter?.code}`"
             >
-                <div>{{ getTranslatedProperty(filter, 'name') }}</div>
-                <div v-if="filter.code === 'properties'" class="border flex flex-col">
-                    <label v-for="option in filter.options" :key="option.id">
-                        <input
-                            v-model="selectedFilters.properties"
-                            type="checkbox"
-                            :value="option.id"
-                            @change="onFilterChange()"
-                        >
-                        {{ getTranslatedProperty(option, 'name') }}
-                    </label>
-                </div>
-                <div v-if="filter.code === 'manufacturer'" class="border flex flex-col">
-                    <label v-for="entity in filter.entities" :key="entity.id">
-                        <input
-                            v-model="selectedFilters.manufacturer"
-                            type="checkbox"
-                            :value="entity.id"
-                            @change="onFilterChange()"
-                        >
-                        {{ getTranslatedProperty(entity, 'name') }}
-                    </label>
-                </div>
+                <ComponentDropdown
+                    :trigger-label="getTranslatedProperty(filter, 'name')"
+                    class="w-full border hover:bg-gray-100 btn-small justify-between"
+                    content-classes="bg-white w-full p-2 lg:origin-top-left lg:absolute lg:left-0 lg:mt-2 lg:w-56 lg:rounded-sm lg:shadow-lg lg:border"
+                >
+                    <div v-if="filter.code === 'properties'" class="flex flex-col">
+                        <label v-for="option in filter.options" :key="option.id">
+                            <input
+                                v-model="selectedFilters.properties"
+                                type="checkbox"
+                                :value="option.id"
+                                @change="onFilterChange()"
+                            >
+                            {{ getTranslatedProperty(option, 'name') }}
+                        </label>
+                    </div>
+                    <div v-if="filter.code === 'manufacturer'" class="flex flex-col">
+                        <label v-for="entity in filter.entities" :key="entity.id">
+                            <input
+                                v-model="selectedFilters.manufacturer"
+                                type="checkbox"
+                                :value="entity.id"
+                                @change="onFilterChange()"
+                            >
+                            {{ getTranslatedProperty(entity, 'name') }}
+                        </label>
+                    </div>
+                </ComponentDropdown>
             </div>
         </div>
         <template #fallback>
-            <div class="flex gap-3">
+            <div class="hidden lg:flex lg:gap-3">
                 Loading...
             </div>
         </template>
-    </client-only>
+    </LayoutSidenavOverlay>
 </template>
