c8b86a04f41357771ef2f9d4e8993f675a1491e9
Lukas Heinze <heinze@digitalmanufaktur.com>
2025-04-15 11:14:20 +0200

    Add missing German translation files

--- i18n/de-DE/index.ts
+++ i18n/de-DE/index.ts
@@ -3,6 +3,10 @@ import cart from './cart.json'
 import currency from './currency.json'
 import form from './form.json'
 import checkout from './checkout.json'
+import header from './header.json'
+import search from './search.json'
+import sidenav from './sidenav.json'
+import megaMenu from './megaMenu.json'
 
 export default {
     ...layout,
@@ -10,4 +14,8 @@ export default {
     ...currency,
     ...form,
     ...checkout,
+    ...header,
+    ...search,
+    ...sidenav,
+    ...megaMenu,
 }
