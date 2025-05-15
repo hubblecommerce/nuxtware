c00c77c45303d05e9c7af8f5b0e655fb7a15f5f0
Lukas Heinze <heinze@digitalmanufaktur.com>
2025-05-15 16:04:56 +0200

    Implement ProductListingFilterShippingFree with toggle switch

--- app/components/product/ProductListingFilterShippingFree.vue
+++ app/components/product/ProductListingFilterShippingFree.vue
@@ -1,6 +1,38 @@
 <script setup lang="ts">
+import type { Schemas } from '#shopware';
+
+interface ListingFilter {
+    code: string;
+    label: string;
+}
+
+const emits = defineEmits<(e: 'select-value', value: unknown) => void>();
+
+const props = defineProps<{
+    filter: ListingFilter;
+    selectedFilters: Schemas['ProductListingResult']['currentFilters'];
+}>();
+
+// Handle shipping-free toggle change
+const onToggleChange = () => {
+    // Directly update the selectedFilters object
+    // Note: Parent component expects direct mutation of this reactive object
+    // eslint-disable-next-line vue/no-mutating-props
+    props.selectedFilters['shipping-free'] = !props.selectedFilters['shipping-free'];
+    emits('select-value', {});
+};
+
+// Computed property to get current filter state
+const isShippingFree = computed(() => !!props.selectedFilters['shipping-free']);
 </script>
 
 <template>
-    <div />
+    <div class="shipping-free-filter py-2 px-4">
+        <ComponentToggleSwitch
+            :model-value="isShippingFree"
+            :label="filter.label"
+            theme="toggle-primary"
+            @change="onToggleChange"
+        />
+    </div>
 </template>
