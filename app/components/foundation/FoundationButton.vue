a12354f2a92d975eb87a151f3ee7432ddb6c061b
Lukas Heinze <heinze@digitalmanufaktur.com>
2025-06-10 08:14:11 +0200

    Added vue imports to FoundationButton.vue

--- app/components/foundation/FoundationButton.vue
+++ app/components/foundation/FoundationButton.vue
@@ -32,7 +32,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, shallowRef, defineEmits, defineExpose, reactive } from 'vue'
 import { useI18n } from 'vue-i18n'
 import type { ButtonProps, ButtonSize, ButtonVariant, ButtonColor } from '#imports'
 
