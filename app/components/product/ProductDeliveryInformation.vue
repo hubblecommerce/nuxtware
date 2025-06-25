dfb3f4a4e1b2ef905288fd161c342ab516cf3cd7
mforster <forster@digitalmanufaktur.com>
2025-06-25 08:38:02 +0200

    feature(BuyBox product delivery information): show product release date

--- app/components/product/ProductDeliveryInformation.vue
+++ app/components/product/ProductDeliveryInformation.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
 import type { Schemas } from "#shopware"
 
+const sessionContextData = ref<Schemas["SalesChannelContext"]>();
+const { languageIdChain } = useSessionContext(sessionContextData.value);
+const { getLanguageCodeFromId } = useInternationalization();
+const locale = getLanguageCodeFromId(languageIdChain.value)
+
 const props = defineProps<{
     product: Schemas["Product"],
 }>()
+
+const isReleaseDateInFuture = computed(() => {
+    const releaseDate = props.product?.releaseDate
+    return !!releaseDate && new Date(releaseDate).getTime() > Date.now()
+})
+
+const formattedReleaseDate = computed(() => {
+    if (!props.product.releaseDate) return ''
+    return new Date(props.product.releaseDate).toLocaleDateString(locale, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    })
+})
 </script>
 <template>
     <div class="product-delivery-information flex flex-col gap-1">
@@ -18,6 +37,13 @@ const props = defineProps<{
             <div class="flex items-center gap-1 text-sm">
                 <div class="w-2 h-2 bg-error rounded-full" />
                 <span>{{ $t('productDeliveryInformation.unavailable') }}</span>
+            </div>
+        </template>
+
+        <template v-else-if="product.releaseDate && isReleaseDateInFuture">
+            <div class="flex items-center gap-1 text-sm">
+                <div class="w-2 h-2 bg-warning rounded-full" />
+                <span>{{ $t('productDeliveryInformation.releaseDate', { date: formattedReleaseDate}) }}</span>
             </div>
         </template>
 
