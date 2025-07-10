a5022795628187bd40a3cc5645b2102eb5c675bb
Christine Lütke <luetke@digitalmanufaktur.com>
2025-07-10 14:23:55 +0200

    add remaining text and image blocks

---
+++ app/components/structure/block/StructureBlockImageTwoColumn.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed, resolveComponent } from 'vue'
+import type { CmsBlockImageTwoColumn } from '@shopware/composables'
+
+const props = defineProps<{
+    content: CmsBlockImageTwoColumn
+}>()
+
+const { getCmsSlot, getCmsElementName } = useCms()
+const leftSlot = computed(() => getCmsSlot(props.content, 'left'))
+const rightSlot = computed(() => getCmsSlot(props.content, 'right'))
+</script>
+
+<template>
+    <div class="grid md:grid-cols-2 gap-10">
+        <component 
+            :is="resolveComponent(getCmsElementName(leftSlot?.type))" 
+            v-if="leftSlot" 
+            :content="leftSlot" 
+        />
+        <component 
+            :is="resolveComponent(getCmsElementName(rightSlot?.type))" 
+            v-if="rightSlot" 
+            :content="rightSlot" 
+        />
+    </div>
+</template>
