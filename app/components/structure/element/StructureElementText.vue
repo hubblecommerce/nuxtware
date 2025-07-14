f10c9ccedbe2c0693dcbca42dde02449048a9d6d
Christine Lütke <luetke@digitalmanufaktur.com>
2025-07-14 07:51:58 +0200

    add prose class and refactor sanitization to use helper for text element

--- app/components/structure/element/StructureElementText.vue
+++ app/components/structure/element/StructureElementText.vue
@@ -7,42 +7,24 @@ const props = defineProps<{
     content: CmsElementText
 }>()
 
+const { sanitizeHtml } = useSanitization()
+
 const textContent = computed(() => {
     return props.content?.data?.content || ''
 })
 
 const sanitizedContent = computed(() => {
-    if (!textContent.value) return ''
-    
-    if (!import.meta.client) {
-        return textContent.value
-    }
-
-    const div = document.createElement('div')
-    div.innerHTML = textContent.value.trim()
-    
-    // Remove potentially harmful attributes
-    const removeAttrs = ['onload', 'onclick', 'onmouseover', 'onmouseout', 'onmousemove', 'onmouseenter', 'onmouseleave', 'onchange', 'onsubmit', 'onreset', 'onfocus', 'onblur']
-    div.querySelectorAll('*').forEach(el => {
-        removeAttrs.forEach(attr => el.removeAttribute(attr))
+    return sanitizeHtml(textContent.value, {
+        removeElements: ['script', 'iframe', 'object', 'embed', 'form', 'input', 'textarea', 'button', 'select', 'option']
     })
-    
-    // Remove potentially harmful elements
-    const removeElements = ['script', 'iframe', 'object', 'embed', 'form', 'input', 'textarea', 'button', 'select', 'option']
-    removeElements.forEach(tagName => {
-        div.querySelectorAll(tagName).forEach(node => node.remove())
-    })
-    
-    return div.innerHTML
 })
 </script>
 
 <template>
-    <!-- eslint-disable-next-line vue/no-v-html -->
     <div 
         v-if="textContent" 
         v-interpolate 
-        class="cms-element-text"
+        class="cms-element-text prose"
         v-html="sanitizedContent"
     />
 </template>
