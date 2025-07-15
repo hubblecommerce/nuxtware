14aad028d833560796ebf967546e7ddb79bb5d41
Christine Lütke <luetke@digitalmanufaktur.com>
2025-07-15 17:37:02 +0200

    feat: implement FrontendLandingPage

--- app/components/frontend/FrontendLandingPage.vue
+++ app/components/frontend/FrontendLandingPage.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts">
+import type { Schemas } from "#shopware"
+
 const props = defineProps<{
-    navigationId: string;
-}>();
+    navigationId: string
+}>()
+
+const { search } = useLandingSearch()
+
+const { data: landingResponse, error } = await useAsyncData(
+    `cmsLanding${props.navigationId}`,
+    async () => {
+        const landingPage = await search(props.navigationId, {
+            withCmsAssociations: true,
+        })
+        return landingPage
+    },
+)
+
+if (!landingResponse?.value) {
+    console.error("[FrontendLandingPage.vue]", error.value?.message)
+    throw createError({
+        statusCode: 500,
+        message: error.value?.message,
+    })
+}
+
+const landingPage = landingResponse as Ref<Schemas["LandingPage"]>
 </script>
 
 <template>
-    <div>
-        Landing: {{ navigationId }}
-    </div>
+    <StructurePage v-if="landingResponse?.cmsPage" :content="landingResponse.cmsPage" />
 </template>
